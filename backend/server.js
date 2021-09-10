4f6c390ca3a546ff51b31c63aca54551d311d05d
NishantChaware <ndchaware99@gmail.com>
2021-09-10 22:42:25 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -21,15 +21,14 @@ const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
-    // app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    // });
-    app.use(express.static(path.resolve(process.cwd(), 'frontend/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(process.cwd(), 'frontend/build/index.html'));
+        res.sendFile(
+            path.resolve(__dirname, 'frontend', 'build', 'index.html')
+        );
     });
+  
 }
 
 app.use(errorHandler);
